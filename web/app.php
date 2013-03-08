880f9f9647952af0216d060f3b88f03e7354241d
Nicolas Bastien <nbastien@prestaconcept.net>
2013-03-08 10:40:13 +0100

    Issue #9: set-up for online demo

--- web/app.php
+++ web/app.php
@@ -1,9 +1,9 @@
 <?php
-
+umask(0000);
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
@@ -12,7 +12,7 @@ $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
 */
 
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
