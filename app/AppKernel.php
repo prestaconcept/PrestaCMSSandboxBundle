f6a950f517ae04bf9ae2ca27c131b5f63059e578
Nicolas Bastien <nbastien@prestaconcept.net>
2013-07-15 12:45:36 +0200

    Set up for Symfony 2.3 and CMF beta 1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,48 +16,46 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
-
+            //Sonata
             new Sonata\BlockBundle\SonataBlockBundle(),
 //            new Sonata\CacheBundle\SonataCacheBundle(),
             new Sonata\jQueryBundle\SonatajQueryBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\SeoBundle\SonataSeoBundle(),
-
-            //Media Bundle
             new Sonata\MediaBundle\SonataMediaBundle(),
-            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
 
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Sonata\DoctrinePHPCRAdminBundle\SonataDoctrinePHPCRAdminBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
 
             // Doctrine PHPCR
             new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
 
-            // support for the admin
-            new Symfony\Cmf\Bundle\RoutingExtraBundle\SymfonyCmfRoutingExtraBundle(),
-            new Symfony\Cmf\Bundle\TreeBrowserBundle\SymfonyCmfTreeBrowserBundle(),
-            new Sonata\DoctrinePHPCRAdminBundle\SonataDoctrinePHPCRAdminBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new Symfony\Cmf\Bundle\BlockBundle\SymfonyCmfBlockBundle(),
-            new Symfony\Cmf\Bundle\ContentBundle\SymfonyCmfContentBundle(),
-            new Symfony\Cmf\Bundle\MenuBundle\SymfonyCmfMenuBundle(),
+            // CMF bundles
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+            new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
+            new Symfony\Cmf\Bundle\MenuBundle\CmfMenuBundle(),
+            new Symfony\Cmf\Bundle\ContentBundle\CmfContentBundle(),
+            new Symfony\Cmf\Bundle\TreeBrowserBundle\CmfTreeBrowserBundle(),
+            new Symfony\Cmf\Bundle\BlockBundle\CmfBlockBundle(),
 
-            new Symfony\Cmf\Bundle\CoreBundle\SymfonyCmfCoreBundle(),
-
-            new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
-            new Application\Presta\CMSCoreBundle\ApplicationPrestaCMSCoreBundle(),
-
+            //PrestaCMS
             new Presta\CMSCoreBundle\PrestaCMSCoreBundle(),
-            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
             new Presta\CMSMediaBundle\PrestaCMSMediaBundle(),
             new Presta\CMSThemeBasicBundle\PrestaCMSThemeBasicBundle(),
+
+            //Utils
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+
+            //PrestaCMS-Sandbox
+            new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
+            new Application\Presta\CMSCoreBundle\ApplicationPrestaCMSCoreBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+
             new Sandbox\ServiceBundle\SandboxServiceBundle(),
         );
 
