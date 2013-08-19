67579b465f417173198c041a0b283009479eb08d
Nicolas Bastien <nbastien@prestaconcept.net>
2013-08-19 11:05:36 +0200

    Refactor to use new theme factory

--- src/Application/Presta/CMSCoreBundle/DataFixtures/PHPCR/LoadTheme.php
+++ src/Application/Presta/CMSCoreBundle/DataFixtures/PHPCR/LoadTheme.php
@@ -47,53 +47,57 @@ class LoadTheme extends AbstractFixture implements ContainerAwareInterface, Orde
      */
     public function load(ObjectManager $manager)
     {
-        $session = $manager->getPhpcrSession();
-
-        //création namespace menu
-        NodeHelper::createPath($session, '/website/sandbox/theme/creative');
-        $root = $manager->find(null, '/website/sandbox/theme/creative');
-
-        $zoneFooterLeft = new Zone('footer_left');
-        $zoneFooterLeft->setParentDocument($root);
-        $manager->persist($zoneFooterLeft);
-
         $ipsumString = '<p>Lorem sequat ipsum dolor lorem sit amet, consectetur adipiscing dolor elit. Aenean nisl orci, condimentum.</p>'
             . '<p>Consectetur adipiscing elit aeneane lorem lipsum, condimentum ultrices consequat eu, vehicula mauris lipsum adipiscing lipsum aenean orci lorem.</p>';
 
-        //Create About Us block
-        $block = new Block();
-        $block->setName('about-us');
-        $block->setParent($zoneFooterLeft);
-        $block->setType('presta_cms.block.simple');
-        $block->setPosition(10);
-        $block->setSetting('title', 'About Us');
-        $block->setSetting('content', $ipsumString);
-        $manager->persist($block);
-        $manager->bindTranslation($block, 'en');
+        $website = $manager->find(null, '/website/sandbox');
 
-        $block->setSetting('title', 'A propos');
-        $block->setSetting('content', $ipsumString);
-        $manager->bindTranslation($block, 'fr');
+        $configuration = array(
+            'name'  => 'creative',
+            'zones' => array(
+                'footer_left' => array(
+                    'name' => 'footer_left',
+                    'blocks' => array(
+                        array(
+                            'type' => 'presta_cms.block.simple',
+                            'position' => 10,
+                            'settings' => array(
+                                'en' => array(
+                                   'title' => 'About Us',
+                                   'content' => $ipsumString
+                                ),
+                                'fr' => array(
+                                    'title' => 'A propos',
+                                    'content' => $ipsumString
+                                )
+                            )
+                        )
+                    )
+                ),
+                'footer_middle' => array(
+                    'name' => 'footer_middle',
+                    'blocks' => array(
+                        array(
+                            'type' => 'presta_cms.block.sitemap',
+                            'position' => 10,
+                            'settings' => array(
+                                'en' => array(
+                                    'title' => 'Useful Links',
+                                    'depth' => 1,
+                                    'root_node' => '/website/sandbox/page/demo'
+                                ),
+                                'fr' => array(
+                                    'title' => 'Liens utiles',
+                                    'depth' => 1,
+                                    'root_node' => '/website/sandbox/page/demo'
+                                )
+                            )
+                        )
+                    )
+                ),
+            )
+        );
 
-        $zoneFooterMiddle = new Zone('footer_middle');
-        $zoneFooterMiddle->setParentDocument($root);
-        $manager->persist($zoneFooterMiddle);
-
-        //Create About Us block
-        $block = new Block();
-        $block->setName('links');
-        $block->setParent($zoneFooterMiddle);
-        $block->setType('presta_cms.block.sitemap');
-        $block->setPosition(10);
-        $block->setSetting('title', 'Useful Links');
-        $block->setSetting('depth', 1);
-        $block->setSetting('root_node', '/website/sandbox/page/demo');
-        $manager->persist($block);
-        $manager->bindTranslation($block, 'en');
-
-        $block->setSetting('title', 'Liens utiles');
-        $manager->bindTranslation($block, 'fr');
-
-        $manager->flush();
+        $this->container->get('presta_cms.theme.factory')->initializeForWebsite($website, $configuration);;
     }
 }
