2af2a245b20fc8d8238642c78ad6a8a40fdf6a27
Nicolas Bastien <nbastien@prestaconcept.net>
2013-08-23 12:13:45 +0200

    Refactor thème fixture in a specific yml file

--- src/Application/Presta/CMSCoreBundle/DataFixtures/PHPCR/LoadTheme.php
+++ src/Application/Presta/CMSCoreBundle/DataFixtures/PHPCR/LoadTheme.php
@@ -18,6 +18,7 @@ use PHPCR\Util\NodeHelper;
 
 use Presta\CMSCoreBundle\Doctrine\Phpcr\Zone;
 use Presta\CMSCoreBundle\Doctrine\Phpcr\Block;
+use Symfony\Component\Yaml\Parser;
 
 /**
  * @author     Nicolas Bastien <nbastien@prestaconcept.net>
@@ -47,56 +48,10 @@ class LoadTheme extends AbstractFixture implements ContainerAwareInterface, Orde
      */
     public function load(ObjectManager $manager)
     {
-        $ipsumString = '<p>Lorem sequat ipsum dolor lorem sit amet, consectetur adipiscing dolor elit. Aenean nisl orci, condimentum.</p>'
-            . '<p>Consectetur adipiscing elit aeneane lorem lipsum, condimentum ultrices consequat eu, vehicula mauris lipsum adipiscing lipsum aenean orci lorem.</p>';
-
         $website = $manager->find(null, '/website/sandbox');
 
-        $configuration = array(
-            'name'  => 'creative',
-            'zones' => array(
-                'footer_left' => array(
-                    'name' => 'footer_left',
-                    'blocks' => array(
-                        array(
-                            'type' => 'presta_cms.block.simple',
-                            'position' => 10,
-                            'settings' => array(
-                                'en' => array(
-                                   'title' => 'About Us',
-                                   'content' => $ipsumString
-                                ),
-                                'fr' => array(
-                                    'title' => 'A propos',
-                                    'content' => $ipsumString
-                                )
-                            )
-                        )
-                    )
-                ),
-                'footer_middle' => array(
-                    'name' => 'footer_middle',
-                    'blocks' => array(
-                        array(
-                            'type' => 'presta_cms.block.sitemap',
-                            'position' => 10,
-                            'settings' => array(
-                                'en' => array(
-                                    'title' => 'Useful Links',
-                                    'depth' => 1,
-                                    'root_node' => '/website/sandbox/page/demo'
-                                ),
-                                'fr' => array(
-                                    'title' => 'Liens utiles',
-                                    'depth' => 1,
-                                    'root_node' => '/website/sandbox/page/demo'
-                                )
-                            )
-                        )
-                    )
-                ),
-            )
-        );
+        $yaml = new Parser();
+        $configuration = $yaml->parse(file_get_contents(__DIR__ . '/../data/creative_theme.yml'));
 
         $this->container->get('presta_cms.theme.factory')->initializeForWebsite($website, $configuration);
     }
