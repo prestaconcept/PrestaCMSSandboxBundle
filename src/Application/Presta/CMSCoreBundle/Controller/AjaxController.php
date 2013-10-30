6fe9fb80d6f346576152629ed1b3b1961d0f08a4
Mathieu Cottet <mcottet@prestaconcept.net>
2013-10-30 11:43:23 +0100

    #23 : add some actions to use the ajax block

---
+++ src/Application/Presta/CMSCoreBundle/Controller/AjaxController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the PrestaCMS Sandbox
+ *
+ * (c) PrestaConcept <http://www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Application\Presta\CMSCoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Mathieu Cottet <mcottet@prestaconcept.net>
+ */
+class AjaxController extends Controller
+{
+    public function parisTimeAction()
+    {
+        return new Response('In Paris, it is ' . $this->getTime('Europe/Paris') . '.');
+    }
+
+    public function laTimeAction()
+    {
+        return new Response('In El Pueblo de Nuestra Señora la Reina de Los Ángeles del Río de Porciúncula, it is ' . $this->getTime('America/Los_Angeles') . '.');
+    }
+
+    /**
+     * @param string $timeZone
+     *
+     * @return string
+     */
+    private function getTime($timeZone)
+    {
+        $date = new \DateTime();
+        $date->setTimezone(new \DateTimeZone($timeZone));
+
+        return $date->format("H:i:s");
+    }
+}
